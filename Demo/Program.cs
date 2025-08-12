cc91bd42dddd6f97fc14f7110cf972d6cb490859
edwar <edwardsameh99@gmail.com>
2025-08-12 16:01:57 +0300

    Vid01 - Vid02

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region What are The Collections?
+            // Collections : Built-in DS in C#
+            // Array: Container
+
+            // Collections in C#
+            // Lists
+            // Hashtables:
+
+            // Generic - Non-Generic
+            // Lists:
+            // Non-Generic Lists: ArrayList - Stack - Queue
+            // Generic Lists    : List, LinkedList, Stack , Queue
+
+            // Hashtables:
+            // Non-Generic HashTables  : HashTable
+            // Generic HashTables      : Dictionary - SortedDictionary 
+            #endregion
         }
     }
 }
